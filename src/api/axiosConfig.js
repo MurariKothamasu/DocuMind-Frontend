e42ef093e584a0847b8cb7e36e2302f54efcf12c
Murari <kothamasuchandrasai56@gmail.com>
2025-11-13 20:55:01 +0530

    added backend url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create an Axios instance
 const api = axios.create({
   // Your backend server URL
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: "https://documind-backend-fv6f.onrender.com",
   
   // IMPORTANT: This allows cookies to be sent with requests
   withCredentials: true, 
