63b9e0a60e89906b0e54542e1be91461539ed42d
Murari <kothamasuchandrasai56@gmail.com>
2025-11-13 19:02:51 +0530

    Changed the Backend Url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create an Axios instance
 const api = axios.create({
   // Your backend server URL
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   
   // IMPORTANT: This allows cookies to be sent with requests
   withCredentials: true, 
